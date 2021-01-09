df6e1710dc550cc39613a8532c9baacd86193324
Rhone Lachner <fernandclay@gmail.com>
2021-01-08 17:14:39 -0800

    find gram by id and top 10 tests passing!

--- __tests__/grams.test.js
+++ __tests__/grams.test.js
@@ -5,6 +5,7 @@ const app = require('../lib/app');
 const UserService = require('../lib/services/Userservice');
 const User = require('../lib/models/User');
 const Comment = require('../lib/models/Comments');
+const Gram = require('../lib/models/Gram');
 
 describe('make a test for the gram routes', () => {
   //USER CREATED FOR TESTING
@@ -116,7 +117,7 @@ describe('make a test for the gram routes', () => {
     });
   });
   // FindBYId test
-  it.only('should find one gram by id, with comments', async() => {
+  it('should find one gram by id, with comments', async() => {
     const gram =
     await agent 
       .post('/api/v1/gram')
@@ -148,6 +149,112 @@ describe('make a test for the gram routes', () => {
     });      
   });
   //findTopTen test
-  
+  it('finds the top 10 popular grams by comments', async() => {
+    const postResponse = await Promise.all(
+      [
+        { userId: user.id, photoUrl: 'http://test.text1.com', caption: 'caption example1', 
+          tags: [{
+            tag1: 'tag1text1',
+            tag2: 'tag2text1'
+          }]
+        },
+
+        { userId: user.id, photoUrl: 'http://test.text2.com', caption: 'caption example2', 
+          tags: [{
+            tag1: 'tag1text2',
+            tag2: 'tag2text2'
+          }]
+        },
+
+        { userId: user.id, photoUrl: 'http://test.text3.com', caption: 'caption example3', 
+          tags: [{
+            tag1: 'tag1text3',
+            tag2: 'tag2text3'
+          }]
+        },
+
+        { userId: user.id, photoUrl: 'http://test.text4.com', caption: 'caption example4', 
+          tags: [{
+            tag1: 'tag1text4',
+            tag2: 'tag2text4',
+            tag3: 'fhuewbaduvkzgfdbuzkcnfaksnfg'
+          }]
+        },
+
+        { userId: user.id, photoUrl: 'http://test.text5.com', caption: 'caption example5', 
+          tags: [{
+            tag1: 'tag1text5',
+            tag2: 'tag2text5',
+            tag3: 'YIPPEEEEEEEE'
+          }]
+        },
+
+        { userId: user.id, photoUrl: 'http://test.text6.com', caption: 'caption example6', 
+          tags: [{
+            tag1: 'tag1text6',
+            tag2: 'tag2text6'
+          }]
+        },
+
+        { userId: user.id, photoUrl: 'http://test.text7.com', caption: 'caption example7', 
+          tags: [{
+            tag1: 'tag1text7',
+            tag2: 'tag2text7'
+          }]
+        },
+
+        { userId: user.id, photoUrl: 'http://test.text8.com', caption: 'caption example8', 
+          tags: [{
+            tag1: 'tag1text8',
+            tag2: 'tag2text8'
+          }]
+        },
+
+        { userId: user.id, photoUrl: 'http://test.text9.com', caption: 'caption example9', 
+          tags: [{
+            tag1: 'tag1text9',
+            tag2: 'tag2text9'
+          }]
+        },
+
+        { userId: user.id, photoUrl: 'http://test.text10.com', caption: 'caption example10', 
+          tags: [{
+            tag1: 'tag1text10',
+            tag2: 'tag2text10'
+          }]
+        },
+
+        { userId: user.id, photoUrl: 'http://test.text11.com', caption: 'caption example11', 
+          tags: [{
+            tag1: 'tag1text11',
+            tag2: 'tag2text11'
+          }]
+        }
+      ].map(gram => Gram.insert(gram)));
+
+    await Promise.all([
+      { userId: '1', gramsId: '1', comment: 'my first comment' },
+      { userId: '1', gramsId: '2', comment: 'my 2nd comment' },
+      { userId: '1', gramsId: '1', comment: 'my 3rd comment' },
+      { userId: '1', gramsId: '1', comment: 'my 4th comment' },
+      { userId: '1', gramsId: '2', comment: 'my 3rd comment' },
+      { userId: '1', gramsId: '8', comment: 'my 4th comment' }
+    ].map(comment => Comment.insert(comment)));
+
+    const expected = [
+      { 'caption': 'caption example1', 'id': '1', 'photoUrl': 'http://test.text1.com', 'tags': [{ 'tag1': 'tag1text1', 'tag2': 'tag2text1' }], 'userId': '1' },
+      { 'caption': 'caption example3', 'id': '2', 'photoUrl': 'http://test.text3.com', 'tags': [{ 'tag1': 'tag1text3', 'tag2': 'tag2text3' }], 'userId': '1' },
+      { 'caption': 'caption example8', 'id': '8', 'photoUrl': 'http://test.text8.com', 'tags': [{ 'tag1': 'tag1text8', 'tag2': 'tag2text8' }], 'userId': '1' }
+    ];
+    
+    const res = await agent
+      .get('/api/v1/gram/topTen');
+    console.log(res.body);
+    expect(expect.arrayContaining(res.body)).toEqual(expect.arrayContaining(
+      expected
+    ));
+    
+  });
 });
+
 
